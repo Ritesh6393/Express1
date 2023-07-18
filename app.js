cf73ec0d827fe3e76025385104cd8f172897c4ef
Ritesh <riteshrai9236@gmail.com>
2023-07-18 11:09:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+const express=require('express');
+
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Welcome to Express!</h1>');
+})
+
+app.listen(3000);
