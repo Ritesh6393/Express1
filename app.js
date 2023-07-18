18c2507f83cd602729b0fe2717fb63392f664c6b
Ritesh6393 <90697437+Ritesh6393@users.noreply.github.com>
2023-07-18 15:56:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 
 
 const express=require('express');
-
+const bodyParser=require('body-parser');
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
+app.use('/addProduct',(req,res,next)=>{
     console.log('In the middleware');
-    next();
+    res.send('<html><head><head><body><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form></body></html>')
 })
-
-app.use((req,res,next)=>{
+  
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
     console.log('In another middleware');
     res.send('<h1>Welcome to Express!</h1>');
 })
